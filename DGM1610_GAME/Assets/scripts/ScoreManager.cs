cbfce8afdce36c13f73e72ba4d5d640a2484412d
Akala <akalamf14@gmail.com>
2018-10-01 17:12:05 -0600

    coin collector

--- DGM1610_GAME/Assets/scripts/ScoreManager.cs
+++ DGM1610_GAME/Assets/scripts/ScoreManager.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class ScoreManager : MonoBehaviour {
+	public static int Score; 
+
+	Text ScoreText;
+
 
 	// Use this for initialization
 	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Score < 0)
+			Score = 0;
+		ScoreText.text = " " + Score; 
 		
 	}
+
+	public static void AddPoints (int PointsToAdd){
+		Score += PointsToAdd; 
+	}
+
 }
+
