6fd8d74dcc395ca9df22fb966e3ab63ec58bf350
Akala <akalamf14@gmail.com>
2018-10-03 20:29:30 -0600

    coding popquiz, and player respawn script

---
+++ DGM1610_GAME/Assets/scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D Player;
+
+	// Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	// Respawn Delay
+	public float RespawnDelay;
+
+	//Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	//Store Gravity Value
+	private float GravityStore;
+
+	//use this for initialization
+	void start(){
+		player = FindObjectOfType<Rigidbody2D> ();
+	}
+
+	public void RespawnPlayer(){
+		StartCoroutine ("RespawnPlayerCo");
+
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//generate death particle
+		Instantiate (DeathParticle, player.transform.position, player.transform.rotation);
+	}
+}
---
+++ DGM1610_GAME/Assets/scripts/PopQuiz.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopQuiz : MonoBehaviour {
+
+	public int x;
+	public int y;
+	private int Total;
+
+	void Start(){
+		PopQuizAdd();
+	}
+
+	public void PopQuizAdd(){
+
+		Add(x, y);
+	}
+
+	public void Add(int x, int y){
+
+		Total = x + y;
+
+		print(Total);
+	}
+
+
+	
+	
+}
