d0973ad616de543903f3b4c226dffb9cde24ee8b
Akala <akalamf14@gmail.com>
2018-10-22 17:06:15 -0600

    projectile script

---
+++ DGM1610_GAME/Assets/scripts/PlayerShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public Transform FirePoint;
+	public GameObject Projectiles;
+
+
+	// Use this for initialization
+	void Start () {
+		Projectiles = GameObject.Find("Projectiles");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightControl))
+			Instantiate(Projectiles, FirePoint.position, FirePoint.rotation);
+	}
+}
---
+++ DGM1610_GAME/Assets/scripts/Projectiles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour {
+
+	public float Speed;
+	public RigidBody2D PC;
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectilesParticle;
+
+	public int PointForKill;
+
+	// Use this for initialization
+	void Start () {
+		if(PC.transform.LocalScale.x < 0)
+			Speed = -Speed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<RigidBody2D>().velocity = new Vector2(Speed, GetComponent<RigidBody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D(collider2D other){
+		if(other.tag == "Enemy"){
+			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+			Destroy(other.GameObject);
+			ScoreManager.AddPoints(PointsForKill);
+		}
+
+		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy(GameObject);
+	}
+}
