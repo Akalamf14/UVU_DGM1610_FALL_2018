0d58b9f99b43d334cc13fb3d8c70f4b43b921965
Akala <akalamf14@gmail.com>
2018-10-24 17:12:42 -0600

    loops script

--- DGM1610_GAME/Assets/scripts/Projectiles.cs
+++ DGM1610_GAME/Assets/scripts/Projectiles.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Projectiles : MonoBehaviour {
 
 	public float Speed;
-	public RigidBody2D PC;
+	public Rigidbody2D PC;
 	public GameObject EnemyDeath;
 
 	public GameObject ProjectilesParticle;
@@ -14,23 +14,23 @@ public class Projectiles : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		if(PC.transform.LocalScale.x < 0)
-			Speed = -Speed
+		if(PC.transform.localScale.x < 0)
+			Speed = -Speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<RigidBody2D>().velocity = new Vector2(Speed, GetComponent<RigidBody2D>().velocity.y);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
-	void OnTriggerEnter2D(collider2D other){
+	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Enemy"){
 			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
-			Destroy(other.GameObject);
-			ScoreManager.AddPoints(PointsForKill);
+			Destroy(other.gameObject);
+			ScoreManager.AddPoints(PointForKill);
 		}
 
-		Instantiate(ProjectileParticle, transform.position, transform.rotation);
-		Destroy(GameObject);
+		Instantiate(ProjectilesParticle, transform.position, transform.rotation);
+		Destroy(gameObject);
 	}
 }
---
+++ DGM1610_GAME/Assets/scripts/practice scripts/Loops.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int num = 40;
+
+
+	// Use this for initialization
+	void Start () {
+		// for(int I = 0; I <= 100; I ++){
+
+		// 	print(I + "Ants Marching");
+	
+		while(num > 0){
+			print("countdown" + num);
+			num --;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
