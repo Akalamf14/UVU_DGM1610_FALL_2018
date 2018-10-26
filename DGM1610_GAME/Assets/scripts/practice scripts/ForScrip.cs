43300b7cedeefcd21c874834f5784ed520dbbb52
Akala <akalamf14@gmail.com>
2018-10-25 21:50:31 -0600

    For loop practice script

---
+++ DGM1610_GAME/Assets/scripts/practice scripts/ForScrip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForPracticeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for(int q = 5; q >25; q --){
+			print("number" + q);
+
+		}
+
+		for(int n = 1; n <= 10; n ++){
+			print("My favorite number is" + n);
+		}
+
+		for(int kiwi = 0; kiwi >= 30; kiwi --){
+			print("this is number" + kiwi);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
