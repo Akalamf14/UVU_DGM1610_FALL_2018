151ebb14ba4aabecbd51c1c25ddfa3dd6a2cf372
Akala <akalamf14@gmail.com>
2018-11-14 13:32:26 -0700

    beginning of health pickup and healthmanager

---
+++ DGM1610_GAME/Assets/scripts/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthMangager : MonoBehaviour {
+
+	public static int Health;
+
+	Text HealthText;
+
+
+	// Use this for initialization
+	void Start () {
+
+		HealthText = GetComponent<Text>();
+		
+		Health = 5;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Health < 1){
+			print("YOU LOSE");
+		}
+		
+	}
+
+	public static void SubtractPoints (int PointsToSubtract){
+		Health -= PointsToSubtract;
+	}
+}
---
+++ DGM1610_GAME/Assets/scripts/HealthPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+	public int PointsToSubtract;
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.GetComponent<Rigidbody2D>() == null)
+			return;
+
+		HealthManager.SubtractPoints(PointsToSubtract);
+
+		Destroy(GameObject);
+	}
+}
